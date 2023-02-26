fcca1cc33204c598e85f1def33c2f5415601c941
KRAMTI INES <ikr@macbook-ns.home>
2023-02-26 21:12:14 +0100

    vieux controller lists

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,20 @@
+class   ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
